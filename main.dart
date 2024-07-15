c3ce46c94abb8983ca3ed253e03e5cefb52fbe93
maurojh <maurojh@gmail.com>
2024-07-15 16:57:48 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_jogar_dados/resposta_estado.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: RespostaEstado(),
+        ),
+      ),
+    );
+  }
+}
+
+
